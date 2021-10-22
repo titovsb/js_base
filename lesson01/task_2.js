3686c6860b265619b682aff341a11afba3c28714
Sergey Titov <titovsb@gmail.com>
2021-10-22 11:36:32 +0300

    Добавлены 2 3 задания

---
+++ lesson01/task_2.js
@@ -0,0 +1,11 @@
+/*
+2 Объявить две переменные: admin и name. Записать в name строку "Василий". Скопировать
+значение из name в admin. Вывести в консоль переменную admin (должно вывести "Василий").
+*/
+
+'use strict';
+let admin = null;
+let $name = null;       // name зарезервировано
+admin = 'Василий';
+$name = admin;
+console.log($name);
